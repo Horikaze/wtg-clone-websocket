b27d89831381b90f24fd07f33e39296c5eb19f12
Horikaze <adrianyt659@gmail.com>
2023-09-01 13:18:58 +0200

    update

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,16 +1,16 @@
 import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
-
 export default function Player() {
   const player = useRef<ReactPlayer>(null);
   const [playing, setPlaying] = useState(true);
   const [URL, setURL] = useState("");
   const [duration, setDuration] = useState<number | null>(null);
   const [currentTime, setCurrentTime] = useState<number>(0);
-
+  
   // const togglePlay = () => {
   //   setPlaying((prevPlaying) => !prevPlaying);
   // };
+
 
   const handleSeekChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newTime = parseFloat(e.target.value);
@@ -45,7 +45,7 @@ export default function Player() {
           }}
         />
       </div>
-      <div className="relative my-2 mx-2">
+      <div className="my-2 mx-2">
         <input
           type="range"
           min={0}
@@ -55,7 +55,7 @@ export default function Player() {
           className="appearance-none  w-full bg-transparent [&::-webkit-slider-runnable-track]:rounded-sm [&::-webkit-slider-runnable-track]:bg-black/30 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:h-[20px] [&::-webkit-slider-thumb]:w-[10px] [&::-webkit-slider-thumb]:rounded-sm [&::-webkit-slider-thumb]:bg-white"
         />
         <div className="flex flex-row justify-between items-center">
-          <div className="w-1/2">
+          <div className="w-1/2 relative">
             <input
               className="w-full bg-black/30 text-gray-300 px-2 py-2 focus:outline-none"
               type="text"
@@ -64,6 +64,13 @@ export default function Player() {
                 setURL(e.target.value);
               }}
             />
+            <button
+              className="absolute right-0 p-2 bg-gray-500 hover:brightness-125 text-white"
+              onClick={() => {
+              }}
+            >
+              Meow
+            </button>
           </div>
           <div className="text-gray-300">
             <p>
--- client/src/components/RightPanel.tsx
+++ client/src/components/RightPanel.tsx
@@ -1,12 +1,21 @@
 import UserList from "./UserList";
 
 export default function RightPanel() {
-  const users = ["asdfdwsf", "asasdw qd", "sdfsdfgsrdgw4e"];
+  const users = ["Hrkz", "asasdw qd", "sdfsdfgsrdgw4e"];
   return (
-    <div className="w-2/12 bg-slate-800 rounded-r-lg min-h-screen pt-1">
+    <div className="w-2/12 bg-slate-800 rounded-r-lg min-h-screen pt-1 px-1">
       <p className="text-gray-200 text-lg font-bold text-center">Users</p>
-      <input type="text" />
-      <div className="flex flex-col gap-1 m-1">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Nickname"
+          className="focus:outline-none p-1 absolute"
+        />
+        <button className="bg-gray-400 hover:brightness-125 p-1 absolute right-0">
+          OK
+        </button>
+      </div>
+      <div className="flex flex-col gap-1">
         {users.map((user) => {
           return <UserList user={user} key={user} />;
         })}
